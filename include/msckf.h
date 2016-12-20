4a882ffb2d1012479072efb206169db2631e4d35
doomzzju <dongxuanlizju@gmail.com>
2016-12-20 16:12:48 +0800

    add main and msckf class

---
+++ include/msckf.h
@@ -0,0 +1,15 @@
+#ifndef MSCKF_H
+#define MSCKF_H
+
+#include <iostream>
+
+// The main class of MSCKF 
+class Msckf
+{
+public:    
+    
+private:
+    
+};
+
+#endif //MSCKF_H
---
+++ src/msckf.cpp
@@ -0,0 +1,4 @@
+#include "msckf.h"
+
+Msckf::Msckf(){}
+
